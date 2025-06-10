5003bb9258905aa3b8d6162cd8ef8610fd918241
Marco Bispo <marcobispo97@gmail.com>
2025-06-09 23:17:48 -0300

    Starting tic tac toe

---
+++ S12_Tic_Tac_Toe/main.py
@@ -0,0 +1,38 @@
+class Move:
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+    
+    def is_valid(self):
+        return 1 <= self._value <= 9
+    
+    def get_row(self):
+        if self._value in [1, 2, 3]:
+            return 0 #Finish row
+        elif self._value in [4, 5, 6]:
+            return 1
+        else: 
+            return 2
+
+    def get_column(self):
+        if self._value in [1, 4, 7]:
+            return 0
+        elif self._value in [2, 5, 8]:
+            return 1
+        else:
+            return 2
+
+# |1|2|3|
+# |4|5|6|
+# |7|8|9|
+move = Move(5)
+print(move.is_valid())  # Output: True
+print(move.value)  # Output: 5
+print(move.get_row())  # Output: 1
+print(move.get_column())  # Output: 1
+move = Move(10)
+print(move.is_valid())  # Output: True
+print(move.value) 
