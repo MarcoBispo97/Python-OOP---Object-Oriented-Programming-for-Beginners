5d8821c64710c0804682cd09fce14647fca2b73f
Marco Bispo <marcobispo97@gmail.com>
2025-06-15 15:26:50 -0300

    tic tac toe all code

--- S12_Tic_Tac_Toe/move.py
+++ S12_Tic_Tac_Toe/move.py
@@ -1,30 +1,27 @@
 class Move:
+ 
     def __init__(self, value):
         self._value = value
-
+ 
     @property
     def value(self):
         return self._value
-    
+ 
     def is_valid(self):
         return 1 <= self._value <= 9
-    
+ 
     def get_row(self):
-        if self._value in [1, 2, 3]:
-            return 0 #Finish row
-        elif self._value in [4, 5, 6]:
-            return 1
-        else: 
-            return 2
-
-    def get_column(self):
-        if self._value in [1, 4, 7]:
+        if self.value in (1, 2, 3):
             return 0
-        elif self._value in [2, 5, 8]:
+        elif self.value in (4, 5, 6):
             return 1
         else:
             return 2
-
-# |1|2|3|
-# |4|5|6|
-# |7|8|9|
+    
+    def get_column(self):
+        if self.value in (1, 4, 7):
+            return 0
+        elif self.value in (2, 5, 8):
+            return 1
+        else:
+            return 2
--- S12_Tic_Tac_Toe/player.py
+++ S12_Tic_Tac_Toe/player.py
@@ -1,52 +1,58 @@
-import random
 from move import Move
-
+import random
+from board import Board # Import Board to use Board.EMPTY_CELL
 
 class Player:
-
-    PLAYER_MARKER = 'X'
-    COMPUTER_MARKER = 'O'
-
+    PLAYER_MARKER = "X"
+    COMPUTER_MARKER = "O"
+ 
     def __init__(self, is_human=True):
         self._is_human = is_human
-
+ 
         if is_human:
             self._marker = Player.PLAYER_MARKER
         else:
             self._marker = Player.COMPUTER_MARKER
-
+ 
     @property
     def is_human(self):
         return self._is_human
-    
+ 
     @property
     def marker(self):
         return self._marker
-    
+ 
     def get_move(self):
         if self._is_human:
             return self.get_human_move()
         else:
             return self.get_computer_move()
-        
+            # Para o design atual, vamos passar o board em ticTacToeGame.py
+
     def get_human_move(self):
         while True:
-            user_input = int(input("Please enter your move (1-9): "))
-            move = Move(user_input)
-            if move.is_valid():
-                break
-            else:
-                print("IPlease enter a number between 1 and 9.")
+            try:
+                user_input = int(input(f"Player {self._marker}, please enter your move (1-9): "))
+                move = Move(user_input)
+                if move.is_valid():
+                    break
+                else:
+                    print("Invalid input. Please enter an integer between 1 and 9.")
+            except ValueError:
+                print("Invalid input. Please enter an integer.")
         return move
+ 
+    def get_computer_move(self, board_instance): # Aceita a instância do tabuleiro
+        available_moves = []
+        for r_idx, row in enumerate(board_instance.game_board):
+            for c_idx, cell in enumerate(row):
+                if cell == Board.EMPTY_CELL:
+                    available_moves.append(r_idx * 3 + c_idx + 1)
+        
+        if not available_moves: # Não deve acontecer se check_is_tie for chamado antes
+            return None
 
-    def get_computer_move(self):
-        random_choice = random.choice(range(1, 10))
-        move = Move(random_choice)
-        print(f"Computer chose move (1-9): {move.value}")
-        return Move(1, 9)
-    
-# player = Player(False) #Human Player
-# move = player.get_move()
-# print(move.value)
-# print(player.is_human)
-# print(player.marker)
+        chosen_value = random.choice(available_moves)
+        move = Move(chosen_value)
+        print(f"Computer ({self.marker}) chose move: {move.value}")
+        return move
